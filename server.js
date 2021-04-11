07df17a6575a091cac51579f9ca200bf867dade1
unknown <kowalczyk.przemek@wp.pl>
2021-04-12 00:27:57 +0200

    Add structure

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({
+    "origin": "https://kodilla.com", //origin sets domains that we approve
+    "methods": "GET,POST", //we allow only GET and POST methods
+  }));
+
+  
+
 app.listen(3000, () => {
   console.log('Server is running on port: 3000');
 });
