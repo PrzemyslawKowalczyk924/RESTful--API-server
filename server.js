20c9fdcf5f8d20d4cff67be9448ab6bb2bbecada
unknown <kowalczyk.przemek@wp.pl>
2021-04-14 00:49:02 +0200

    Add client project

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-//const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
 //const path = require('path');
 const app = express();
@@ -24,6 +23,6 @@ app.use((req, res) => {
     res.status(404).send('404 not found...');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port: 3000');
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
 });
