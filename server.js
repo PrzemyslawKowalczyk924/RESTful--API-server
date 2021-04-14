a09ceb623d076c1baed207bb97fddce949e56b3b
unknown <kowalczyk.przemek@wp.pl>
2021-04-14 23:18:43 +0200

    Modify server for upcoming publishing on heroku

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-//const path = require('path');
+const path = require('path');
 const app = express();
-const db = require('./db');
+
+//import routes
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertRoutes);
 app.use('/api', seatsRoutes);
 
-app.get('/', (req, res) => {
-  res.json(db);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
